1c9402bf2c3d68877a870c1ad36c47cb0b78c8b9
John Hoffman <gnosisadvent@live.com>
2026-04-06 16:34:30 -0500

    Solution amended for decimal typo

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -9,7 +9,7 @@
             char letterOfTheDay = 'G';
             bool nah = false;
             double dub64 = 9001;
-            decimal dec64 = 1000000;
+            decimal dec64 = 1000000m;
             
             Console.WriteLine($"{initialGreeting} It has been {xDays} since your last visit.\nThe letter of the day is {letterOfTheDay}\n" +
                               $"You tell your magic 8-ball that the outlook for the day is good. It responds with \"Your assesment is {nah}.\"\n" +
--- VariablesExercise/obj/Debug/net9.0/VariablesExercise.AssemblyInfo.cs
+++ VariablesExercise/obj/Debug/net9.0/VariablesExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VariablesExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e474f56b996945407d5e52be748b4e139fe9b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b00f24518f880d3f873d272136b6d582fa0b95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VariablesExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VariablesExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
