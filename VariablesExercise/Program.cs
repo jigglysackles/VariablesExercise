1b00f24518f880d3f873d272136b6d582fa0b95c
John Hoffman <gnosisadvent@live.com>
2026-04-06 14:20:51 -0500

    Solution Completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string initialGreeting =  "Hello, World!";
+            int xDays = 3;
+            char letterOfTheDay = 'G';
+            bool nah = false;
+            double dub64 = 9001;
+            decimal dec64 = 1000000;
+            
+            Console.WriteLine($"{initialGreeting} It has been {xDays} since your last visit.\nThe letter of the day is {letterOfTheDay}\n" +
+                              $"You tell your magic 8-ball that the outlook for the day is good. It responds with \"Your assesment is {nah}.\"\n" +
+                              $"Your power level today is {dub64} and you have a ransom demand of {dec64} dollars.\n");
         }
     }
 }
